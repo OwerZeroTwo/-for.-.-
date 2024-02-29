edcad397027f731365cc53d54b5691dd3caaf1eb
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-02-29 14:49:09 +0300

    Add files via upload

---
+++ Цикл for. Элементы списка. Полезные функции в цикле.py
@@ -0,0 +1,6 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(len(cars)):
+    cars_count += 10
+    print('Я езжу на автомабиле марки', cars[i], )
+    print(cars_count)
